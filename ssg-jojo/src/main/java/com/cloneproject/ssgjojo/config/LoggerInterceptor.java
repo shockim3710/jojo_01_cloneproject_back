5d78b76197b14918cbce5211de29cff225a35d43
Bear-Sik <gs.min.1226@gmail.com>
2022-09-06 23:59:12 +0900

    LOG 추가 및 gitignore 업데이트

---
+++ ssg-jojo/src/main/java/com/cloneproject/ssgjojo/config/LoggerInterceptor.java
@@ -0,0 +1,27 @@
+package com.cloneproject.ssgjojo.config;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Slf4j
+@Component
+public class LoggerInterceptor extends HandlerInterceptorAdapter{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		log.warn("======================================          START         ======================================");
+		log.warn(" Request URI \t:  " + request.getRequestURI());
+		return super.preHandle(request, response, handler);
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		log.warn("======================================           END          ======================================\n");
+	}
+}
+
